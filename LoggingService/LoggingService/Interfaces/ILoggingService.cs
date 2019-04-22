d52bb98b323937c73010886f4cec771c22461db5
Richard Kneale <richard@mantaraymedia.co.uk>
2019-04-22 18:14:21 +0100

    Updated LoggingService

---
+++ LoggingService/LoggingService/Interfaces/ILoggingService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace LoggingService.Interfaces
+{
+    public interface ILoggingService
+    {
+        void WriteToLoggingFile(string loggingMessageType, string loggingMessage);
+
+        void WriteToLoggingFile(string loggingMessageType, string loggingMessage, StackFrame stackFrame);
+
+        void WriteToLoggingFile(string loggingMessageType, string loggingMessage, string stackFrame);
+
+        void WriteToLoggingFile(Exception loggingMessage);
+    }
+}
---
+++ LoggingService/LoggingService/Models/LoggingModel.cs
@@ -0,0 +1,9 @@
+﻿namespace LoggingService.Models
+{
+    public class LoggingModel
+    {
+        public int DaysToLog { get; set; }
+
+        public string Directory { get; set; }
+    }
+}
